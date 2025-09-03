f8f0ebdf6a9ba20e12ecb872647eab73c0804913
mariaquintero2818-boop <mariaquintero2818@gmail.com>
2025-09-03 17:58:29 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,13 @@
+/* 
+Maria Quintero
+COSC 1336 001
+09/03/2025
+Purpose: To print out "Hello World!" (Lab0L1 - Hello, World!)
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
